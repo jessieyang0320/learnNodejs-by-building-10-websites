ff18a034cf1886863f8b1a0b81e938da726760d7
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-05-13 13:26:12 -0400

    add comments feature

--- node-blog/nodeblog/routes/posts.js
+++ node-blog/nodeblog/routes/posts.js
@@ -94,5 +94,70 @@ router.post('/add', upload.single('mainimage'), function(req, res, next) {
 
 
 });
+
+// add comments to a post
+
+router.post('/addcomment', function(req, res, next) {
+// get form values
+
+	var name = req.body.name;
+	var email = req.body.email;
+	var body = req.body.body;
+	var postid = req.body.postid;
+	var commentdate = new Date();
+
+	
+
+	//  Form validations
+
+	req.checkBody('name','Name field is required').notEmpty();
+	req.checkBody('body','Body field is required').notEmpty();
+	req.checkBody('email','Email field is required').notEmpty();
+	req.checkBody('email','Email should be valid').isEmail();
+
+	// check errors
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		var posts = db.get('posts');
+		posts.findById(postid, function(err,post){
+		  res.render('show',{
+			"errors": errors,
+			"post":post
+
+		    })			
+		})
+
+	} else {
+		var comment = {
+			"name" : name,
+			"email" : email,
+			"body" : body,
+			"commentdate" : commentdate
+		}
+
+		var posts = db.get('posts');
+		posts.update({
+			"_id": postid
+		},{
+			$push:{
+				"comments":comment
+			}
+		}, function(err, doc){
+			if(err){
+				throw err;
+			} else {
+				req.flash('success','comment added');
+				res.location('/posts/show/'+postid);
+				res.redirect('/posts/show/'+postid);
+			}
+
+		})
+		
+	}
+
+
+});
  
 module.exports = router;
